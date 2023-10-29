74182684750277d3e77dc5867a2357b68f719029
ATTAQA <pramoddutta_96@yahoo.com>
2023-10-29 12:55:51 +0530

    OCT

--- src/main/java/com/thetestingacademy/utils/FillowUtil.java
+++ src/main/java/com/thetestingacademy/utils/FillowUtil.java
@@ -7,14 +7,15 @@ import com.codoid.products.fillo.Recordset;
 
 public class FillowUtil {
 
-    public static String FILEPATH = System.getProperty("user.dir")+"/src/main/java/com/thetestingacademy/resources/TDF.xlsx";
+    public static String FILEPATH = System.getProperty("user.dir")+"/src/main/java/com/thetestingacademy/resources/TDNN.xlsx";
 
 
     public static String fetchDataFromXLSX(String sheetName,String id, String fieldName) throws FilloException {
         String value = null;
         Fillo fillo = new Fillo();
         Connection connection =  fillo.getConnection(FILEPATH);
-        String query = "Select * from " + sheetName + "" + " where ID=" + id + "";
+        String query = "Select * from " + sheetName + " " + "where ID='" + id + "'";
+
         Recordset recordset = connection.executeQuery(query);
         while(recordset.next()){
             value = recordset.getField(fieldName);
